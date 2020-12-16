e28dad2eeaab7c2aacd44689e56157f1442ad85a
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2020-12-16 23:54:35 +0700

    Added WSGI file

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""
+Created on 16/12/2020
+
+@author: Gregorius Ivan Sebastian
+@office: Sprint Asia Technology
+@email: greg.sebastian@sprintasia.co.id / ivansebastian60@gmail.com
+"""
+from app import app
+
+app.run()
